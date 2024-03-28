40a1398f3908e91aa1e1379f4477f68e9e9385b8
cimkoreauniv <changilmoon@korea.ac.kr>
2024-03-28 14:53:02 +0900

    ~240328

--- 24_03/12851.cpp
+++ 24_03/12851.cpp
@@ -1,10 +1,41 @@
 #include <stdio.h>
+#include <memory.h>
 #include <queue>
 
-std::pair<int, int> vis[100010];
+struct element
+{
+    int time, cnt;
+} vis[100010];
 
 int main(void)
 {
     int N, K;
+    std::queue<int> q;
     scanf("%d %d", &N, &K);
+    vis[N] = {0, 1};
+    q.push(N);
+    while (!q.empty())
+    {
+        int now = q.front();
+        q.pop();
+        if (vis[K].cnt > 0 && vis[now].time > vis[K].time)
+            break;
+        int nowtime = vis[now].time, nowcnt = vis[now].cnt;
+        int targets[] = {now - 1, now + 1, now * 2};
+        int check[] = {now > 0, now < 100000, now <= 50000};
+        for (int i = 0; i < 3; i++)
+            if (check[i])
+            {
+                element &next = vis[targets[i]];
+                if (next.cnt == 0)
+                {
+                    next.time = nowtime + 1;
+                    q.push(targets[i]);
+                }
+                if (next.time == nowtime + 1)
+                    next.cnt += nowcnt;
+            }
+    }
+    printf("%d\n%d", vis[K].time, vis[K].cnt);
+    return 0;
 }
